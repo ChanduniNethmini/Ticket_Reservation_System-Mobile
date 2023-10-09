76089656e8f3bc7995a408a49bc01b8ff43c8fec
ChanduniNethmini <nchanduni21@gmail.com>
2023-10-10 03:14:44 +0530

    modified home screen and add reservation

--- app/src/main/java/com/example/ticketmanagementsystem/Dashboard.java
+++ app/src/main/java/com/example/ticketmanagementsystem/Dashboard.java
@@ -2,15 +2,43 @@ package com.example.ticketmanagementsystem;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class Dashboard extends AppCompatActivity {
+    private Button button, button2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_dashboard);
+
+        button = (Button) findViewById(R.id.button2);
+        button2 = (Button) findViewById(R.id.button1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
     }
+    public void openActivity2(){
+        Intent intent = new Intent(this, ViewBooking.class);
+        startActivity(intent);
+    }
+    public void openActivity3(){
+        Intent intent = new Intent(this, DisplayBookings.class);
+        startActivity(intent);
+    }
+
 }
